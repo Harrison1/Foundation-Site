2f778e139a1410eb6ca7507cd5ba13ea3a7ae2c0
Harrison <hmcguire13@gmail.com>
2016-05-14 18:10:17 -0700

    added react lazy load and css fadin

--- js/components/OtherProductItem.js
+++ js/components/OtherProductItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import LazyLoad from 'react-lazyload';
+
 export default class extends React.Component{
 	render() {
 		return (
 				<div class="media-object">
 					<div class="media-object-section">
-						<img class="thumbnail lazy" src={this.props.promoimg} />
+						<LazyLoad height={108} once>
+							  <img class="productfade" src={this.props.promoimg} />
+						</LazyLoad>	
 					</div>
 					<div class="media-object-section">
 						<h5>{this.props.title}</h5>
